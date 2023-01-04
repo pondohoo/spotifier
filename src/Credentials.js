901897e9e9d07ac58bc08d97ecf57c9c2eccc8b8
theo <theobfernandez@gmail.com>
2023-01-03 19:25:57 -0800

    Fix redirect bug

--- src/Credentials.js
+++ src/Credentials.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const authEndpoint = "https://accounts.spotify.com/authorize?"
 const clientId = "e6b7fef72db04e2ba23e4e61412199ec"
-const redirectUri = "http://localhost:3000";
+const redirectUri = "https://spotifierstats.vercel.app/";
 const scopes = ["user-library-read", "playlist-read-private", "user-top-read"];
 
 export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
